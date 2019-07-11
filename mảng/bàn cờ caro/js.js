01aa67ae98acbc297fd84ddf7cefc1cf9d238887
Nguyễn Quốc Báo <nqb.10i1@gmail.com>
2019-07-11 20:06:17 +0700

    add thuc hanh mang

---
+++ mảng/bàn cờ caro/js.js
@@ -0,0 +1,40 @@
+let b = document.getElementById("dauvao");
+
+let board = [];
+var data = "";
+for (let i = 0; i < 5; i++) {
+    board[i] = new Array(0, 0, 0, 0, 0);
+}
+data += "<table style='margin:0px auto' border='1' width='300' cellspacing='0' cellpadding='3'>"
+for (let i = 0; i < 5; i++) {
+    data += "<tr>";
+    for (let j = 0; j < 5; j++) {
+        data += "<td>"+board[i][j]+ "</td>" ;
+    }
+    data += "</tr>"
+}
+data += "</table>" 
+
+b.innerHTML = data + "<br/><br/>";
+
+function changeValue(){
+    let diemX = +prompt('X: ');
+    let diemY = +prompt('Y: ');
+    
+    data = '';
+    data += "<table style='margin:0px auto' border='1' width='300' cellspacing='0' cellpadding='3'>"
+    for (let i = 0; i < 5; i++) {
+        data += "<tr>";
+        for (let j = 0; j < 5; j++) {
+            if((i +1) === diemX && (j +1)  === diemY){
+                board[i][j] = 'x';
+                
+            }
+            data +="<td>"+ board[i][j]+ "</td>";
+        }
+        data += "</tr>"
+    } 
+    data += "</table>" 
+    b.innerHTML = data +"<br/><br/>";
+}
+
