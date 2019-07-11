4965f93712e6b2fac8dbca3531ec147788022f6c
Nguyễn Quốc Báo <nqb.10i1@gmail.com>
2019-07-11 20:24:51 +0700

    add thuc hanh mang1

--- mảng/bàn cờ caro/js.js
+++ mảng/bàn cờ caro/js.js
@@ -1,5 +1,4 @@
 let b = document.getElementById("dauvao");
-
 let board = [];
 var data = "";
 for (let i = 0; i < 5; i++) {
@@ -14,21 +13,18 @@ for (let i = 0; i < 5; i++) {
     data += "</tr>"
 }
 data += "</table>" 
-
 b.innerHTML = data + "<br/><br/>";
 
 function changeValue(){
     let diemX = +prompt('X: ');
     let diemY = +prompt('Y: ');
-    
     data = '';
     data += "<table style='margin:0px auto' border='1' width='300' cellspacing='0' cellpadding='3'>"
     for (let i = 0; i < 5; i++) {
         data += "<tr>";
         for (let j = 0; j < 5; j++) {
             if((i +1) === diemX && (j +1)  === diemY){
-                board[i][j] = 'x';
-                
+                board[i][j] = 'x';     
             }
             data +="<td>"+ board[i][j]+ "</td>";
         }
